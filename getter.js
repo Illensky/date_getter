b406daf383b2a4cf6b89d92f0127743828d6321f
Illensky <alexis.laroche.02240@gmail.com>
2021-11-29 15:34:09 +0100

    reminder writted

---
+++ getter.js
@@ -0,0 +1,17 @@
+let result = document.getElementById("result")
+
+let aujourdhui = new Date()
+
+result.innerHTML =
+    "Jour de la semaine : " + aujourdhui.getDay() + "<br>" +
+    "Jour du mois : " + aujourdhui.getDate() + "<br>" +
+    "mois en cours version JS : " + aujourdhui.getMonth() + "<br>" +
+    "Mois en cours version humaine : " + (aujourdhui.getMonth() +1) + "<br>" +
+    "Année au format 4 chiffres : " + aujourdhui.getFullYear() + "<br>" +
+    "Année au format 2 chiffres : " + aujourdhui.getFullYear().toString().slice(2) + "<br>" +
+    "Heure : " + aujourdhui.getHours() + "<br>" +
+    "Heure avec un zéro devant si besoin : " + aujourdhui.getHours().toString().padStart(2, 0) + "<br>" +
+    "Minutes : " + aujourdhui.getMinutes() + "<br>" +
+    "Minutes avec un zéro devant si besoin : " + aujourdhui.getMinutes().toString().padStart(2, 0) + "<br>" +
+    "Secondes : " + aujourdhui.getSeconds() + "<br>" +
+    "Millisecondes : " + aujourdhui.getMilliseconds();
